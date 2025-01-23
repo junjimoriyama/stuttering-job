ffee5db3f52bacf2f6c039731de158826cfdb0bc
junjimoriyama <pero197030@icloud.com>
2025-01-23 13:49:12 +0900

    メール送信設定済み

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -72,15 +72,15 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   # Gmail設定
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                  587,
-  domain:               'gmail.com',
-  user_name:            '@gmail.com',
-  password:             'pocv nioq dzan saoo',
-  authentication:       'plain',
-  enable_starttls_auto:  true
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  # address:              'smtp.gmail.com',
+  # port:                  587,
+  # domain:               'gmail.com',
+  # user_name:            '@gmail.com',
+  # password:             'pocv nioq dzan saoo',
+  # authentication:       'plain',
+  # enable_starttls_auto:  true
+  # }
 end
 
